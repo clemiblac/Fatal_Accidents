79bf7f25889076de92adac5037f7bc7637c84cdc
clemiblac <48965151+clemiblac@users.noreply.github.com>
2020-08-21 19:36:56 -0400

    Pyramid created

--- static/js/app.js
+++ static/js/app.js
@@ -1,130 +1,29 @@
 
 
 /// route name
-var accidents="/data/fatal";
+var accidents="static/js/fatal_accidents.json";
+//var accidents="/data/fatal";
 
-//////////////HHHHHHH///////////
 d3.json(accidents).then((data)=>{
-    console.log(data);
-    /// Heat map ///
-    
-    /// Day of the week ///
-    //count crashes
-    var week_day=data.map(t=>t.DAY_WEEK)
-    //console.log(week_day)
 
-    var result = { };
-    for(var i = 0; i < week_day.length; ++i) {
-        if(!result[week_day[i]])
-            result[week_day[i]] = 0;
-        ++result[week_day[i]];
-    }
-    //console.log(result)
+    console.log(data)
+        /// Map ///
+    // var map_result = [];
+    // data.reduce(function(res, value) {
+    // if (!res[value.STATE_NAME]) {
+    //     res[value.STATE_NAME] = { STATE_NAME: value.STATE_NAME, FATALS: 0 };
+    //     map_result.push(res[value.STATE_NAME])
+    // }
+    // res[value.STATE_NAME].FATALS += value.FATALS;
+    // return res;
+    // }, {});
 
-
-    var day=Object.keys(result)
-    //.log(day)
-    var count_crashes=Object.values(result)
-    //console.log(count_crashes)
+    // console.log(map_result)
+    // var state=map_result.map(t=>t.STATE_NAME)
+    // var fatal_accidents=map_result.map(t=>t.FATALS)
 
 
 
-    //Sum deaths by day of the week
-    var result = [];
-    data.reduce(function(res, value) {
-    if (!res[value.DAY_WEEK]) {
-        res[value.DAY_WEEK] = { DAY_WEEK: value.DAY_WEEK, DEATHS: 0 };
-        result.push(res[value.DAY_WEEK])
-    }
-    res[value.DAY_WEEK].DEATHS += value.DEATHS;
-    return res;
-    }, {});
-
-    //console.log(result)
-
-    function compare(a, b) {
-        const caseA = a.DAY_WEEK;
-        const caseB = b.DAY_WEEK;
-        
-        let comparison = 0;
-        if (caseA > caseB) {
-            comparison = 1;
-        } else if (caseA < caseB) {
-            comparison = -1;
-        }
-        return comparison * 1;
-    }
-        
-    var sort_weekday=result.sort(compare)
-    //console.log(sort_weekday)
-
-    var count_deaths=sort_weekday.map(t=>t.DEATHS)
-    //console.log(count_deaths)
-
-  
-    var trace1 = {
-        x: day,
-        y: count_crashes,
-        name:"crashes",
-        type: 'scatter'
-      };
-      
-      var trace2 = {
-        x: day,
-        y: count_deaths,
-        name:"deaths",
-        type: 'scatter'
-      };
-      
-      var line_data = [trace1, trace2];
-
-      var layout = {
-        title: {
-            text:'Fatalities by day of the week',
-            font: {
-              family: 'Courier New, monospace',
-              size: 18
-            },
-            xref: 'paper',
-            x: 0.05,
-          },
-        xaxis: {title:"day of the week",
-                size: 18,
-                autotick: false,
-                ticks: 'outside',
-                tick0: 0,
-                dtick: 1,
-                ticklen: 8,
-                tickwidth: 4,
-                tickcolor: '#000'
-            },
-        yaxis: {title:"count by day",automargin: true,},
-        autosize: true,
-        width: 500,
-        height: 500,
-        margin: {
-            l: 100,
-            r: 50,
-            b: 100,
-            t: 100,
-            pad: 4
-        },
-        showlegend: true,
-        legend: {
-            x: 0,
-            y: 1,
-            traceorder: 'normal',
-            font: {
-              family: 'sans-serif',
-              size: 12,
-              color: '#000'
-            },  
-        
-        }
-    }
-    var config = {responsive: true};
-      
-    Plotly.newPlot('day-of-week', line_data,layout,config);
 
     /////////// Time of day
     var time_day=data.map(t=>t.HOUR)
@@ -137,15 +36,233 @@ d3.json(accidents).then((data)=>{
     }
     //console.log(result1)
 
+    /// slice to remove 99th hour which is the number assigned to unknown hour for crash
+    var hour_t=Object.keys(result1).slice(0,24)
+    //console.log(hour_t)
+    var count_crashes_time=Object.values(result1).slice(0,24)
+    //console.log(count_crashes_time)
 
-    var hour_t=Object.keys(result1)
-    console.log(hour_t)
-    var count_crashes_time=Object.values(result1)
-    console.log(count_crashes_time)
 
 
-  
+    var data2 = [{
+        type: 'bar',
+        x: hour_t,
+        y: count_crashes_time,
+        marker:{color:'#FF6347'},
+    }];
+    var layout = {
+        xaxis: {title:"Hour of the day",
+            size: 14,
+            autotick: false,
+            //ticks: 'outside',
+            tick0: 0,
+            dtick: 1,
+            ticklen: 8,
+            tickwidth: 2,
+            tickangle: 45,
+            tickcolor: '#000'},
+        yaxis: {title:"Total crashes",automargin: true,},
+        autosize: true,
+        width: 500,
+        height: 500,
+        margin: {
+            l: 100,
+            r: 50,
+            b: 100,
+            t: 100,
+            pad: 4
+        }
+    };
+    var config = {responsive: true}           
+    Plotly.newPlot('time-of-day', data2,layout,config);
+
+    // /////////////////// GENDER //////////////////////////////
+
+    //First filter dataset by gender
+    male=data.filter(c=>c.SEX==1)
+    female=data.filter(c=>c.SEX==2)
+    console.log("male")
+    //console.log(male)
+
+    /// Males 
+    male0_16=male.filter(c=>c.AGE<=16).length
+    console.log(male0_16)
+    male17_20=male.filter(c=>c.AGE>=17&&c.AGE<=20).length
+    console.log(male17_20)
+    male21_25=male.filter(c=>c.AGE>=21&&c.AGE<=25).length
+    console.log(male21_25)
+    male26_30=male.filter(c=>c.AGE>=26&&c.AGE<=30).length
+    console.log(male26_30)
+    male31_35=male.filter(c=>c.AGE>=31&&c.AGE<=35).length
+    console.log(male31_35)
+    male36_40=male.filter(c=>c.AGE>=36&&c.AGE<=40).length
+    console.log(male36_40)
+    male41_45=male.filter(c=>c.AGE>=41&&c.AGE<=45).length
+    console.log(male41_45)
+    male46_50=male.filter(c=>c.AGE>=46&&c.AGE<=50).length
+    console.log(male46_50)
+    male51_55=male.filter(c=>c.AGE>=51&&c.AGE<=55).length
+    console.log(male51_55)
+    male56_60=male.filter(c=>c.AGE>=56&&c.AGE<=60).length
+    console.log(male56_60)
+    male61_65=male.filter(c=>c.AGE>=61&&c.AGE<=65).length
+    console.log(male61_65)
+    male66_70=male.filter(c=>c.AGE>=66&&c.AGE<=70).length
+    console.log(male66_70)
+    male71_plus=male.filter(c=>c.AGE>=71).length
+    console.log(male71_plus)
+
+    /// Females
+    female0_16=female.filter(c=>c.AGE<=16).length
+    console.log(female0_16)
+    female17_20=female.filter(c=>c.AGE>=17&&c.AGE<=20).length
+    console.log(female17_20)
+    female21_25=female.filter(c=>c.AGE>=21&&c.AGE<=25).length
+    console.log(female21_25)
+    female26_30=female.filter(c=>c.AGE>=26&&c.AGE<=30).length
+    female31_35=female.filter(c=>c.AGE>=31&&c.AGE<=35).length
+    female36_40=female.filter(c=>c.AGE>=36&&c.AGE<=40).length
+    female41_45=female.filter(c=>c.AGE>=41&&c.AGE<=45).length
+    female46_50=female.filter(c=>c.AGE>=46&&c.AGE<=50).length
+    female51_55=female.filter(c=>c.AGE>=51&&c.AGE<=55).length
+    female56_60=female.filter(c=>c.AGE>=56&&c.AGE<=60).length
+    female61_65=female.filter(c=>c.AGE>=61&&c.AGE<=65).length
+    female66_70=female.filter(c=>c.AGE>=66&&c.AGE<=70).length
+    female71_plus=female.filter(c=>c.AGE>=71).length
+
+
+    console.log("Male 17-20")
+    console.log(male17_20)
+
+    var chart = JSC.chart('gender-age', { 
+        debug: true, 
+        type: 'horizontal column', 
+        title_label_text: 
+          'distibution of accidents by sex and age', 
+        yAxis: { 
+          scale_type: 'stacked', 
+          defaultTick_label_text: 
+            '{Math.abs(%Value):a2}'
+        }, 
+        xAxis: { 
+          label_text: 'Age', 
+          crosshair_enabled: true
+        }, 
+        defaultTooltip_label_text: 
+          'Ages %xValue:<br><b>%points</b>', 
+        defaultPoint_tooltip: 
+          '%icon {Math.abs(%Value)}', 
+        legend_template: 
+          '%name %icon {Math.abs(%Value)}', 
+        series: [ 
+          { 
+            name: 'Male', 
+            points: { 
+              mapTo: 'x,y', 
+              data: [ 
+                ['0-16', -male0_16], 
+                ['17-20', -male17_20], 
+                ['21-25', -male21_25], 
+                ['26-30', -male26_30], 
+                ['31-35', -male31_35], 
+                ['36-40', -male36_40], 
+                ['41-45', -male41_45], 
+                ['46-50', -male46_50], 
+                ['51-55', -male51_55], 
+                ['56-60', -male56_60], 
+                ['61-65', -male61_65], 
+                ['66-70', -male66_70], 
+                ['71+', -male71_plus] 
+              ] 
+            } 
+          }, 
+          { 
+            name: 'Female', 
+            points: { 
+              mapTo: 'x,y', 
+              data: [ 
+                ['0-16', female0_16], 
+                ['17-20',female17_20], 
+                ['21-25', female21_25], 
+                ['26-30', female26_30], 
+                ['31-35', female31_35], 
+                ['36-40', female36_40], 
+                ['41-45', female41_45], 
+                ['46-50', female46_50], 
+                ['51-55', female51_55], 
+                ['56-60', female56_60], 
+                ['61-65', female61_65], 
+                ['66-70', female66_70], 
+                ['71+', female71_plus] 
+              ] 
+            } 
+          } 
+        ] 
+      }); 
+
+
+    // /////// LIGHT CONDITIONS ///////////
+
+    /// Count crashes by light conditions
+    var light_conditions=data.map(t=>t.LGT_CONDITIONS)
+    var light_result = { };
+    for(var i = 0; i < light_conditions.length; ++i) {
+        if(!light_result[light_conditions[i]])
+            light_result[light_conditions[i]] = 0;
+        ++light_result[light_conditions[i]];
+    }
+    console.log(light_result)
+
+    /// Sum of fatalities by light conditions
+    var lgt_fatalities_result = [];
+    data.reduce(function(res, value) {
+    if (!res[value.LGT_CONDITIONS]) {
+        res[value.LGT_CONDITIONS] = { LGT_CONDITIONS: value.LGT_CONDITIONS, FATALS: 0 };
+        lgt_fatalities_result.push(res[value.LGT_CONDITIONS])
+    }
+    res[value.LGT_CONDITIONS].FATALS += value.FATALS;
+    return res;
+    }, {});
+
+    console.log(lgt_fatalities_result)
+
+    /// Variables
+    var l_condition=Object.keys(light_result)
+    var l_count_crashes=Object.values(light_result)
+    var l_count_deaths=lgt_fatalities_result.map(t=>t.FATALS)
+    var l_con_fatal=lgt_fatalities_result.map(t=>t.LGT_CONDITIONS)
+
+    var trace1 = {
+        x: l_condition,
+        y: l_count_crashes,
+        name: 'crashes',
+        type: 'bar'
+    };
     
+    var trace2 = {
+        x: l_con_fatal,
+        y: l_count_deaths,
+        name: 'fatalities',
+        type: 'bar'
+    };
+    
+    var light_data = [trace1, trace2];
+    
+    var layout = {barmode: 'group',
+        title: 'Light conditions',
+        autosize: true,
+        width: 500,
+        height: 500,
+        margin: {
+            l: 100,
+            r: 50,
+            b: 100,
+            t: 100,
+            pad: 4
+        }
+    };
+    var config = {responsive: true};
+    Plotly.newPlot('light', light_data, layout,config);
 
 
   
